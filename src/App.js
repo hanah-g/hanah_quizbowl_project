135504eb2b52cb7005db16c994385a0ed0199a1e
Hanah <g.hanah@wustl.edu>
2022-09-14 22:42:48 -0500

    Hanah's QuizBowl Project!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { questions } from './QuestionList'
 
 function App() {
+
+const[showfinalresults, setfinalresults] = useState(false);
+const[score, setScore] = useState(0);
+const[currentQ, setCurrentQ] = useState(0);
+
+const optionClicked = (isCorrect) => {
+  if (isCorrect) {
+    setScore(score + 1);
+  }
+
+  if (currentQ + 1 < questions.length) {
+    setCurrentQ(currentQ + 1);
+  } else {
+    setfinalresults(true);
+  }
+};
+
+/* Resets the game back to default */
+const restartGame = () => {
+  setScore(0);
+  setCurrentQ(0);
+  setfinalresults(false);
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>QuizBowl Practice Test</h1>
+
+     {/*2. Current Score*/ }
+     <h2>Current Score : {score}</h2>
+
+     {showfinalresults ?
+
+     /*3. Final Results*/
+     <div className = 'finalResults'>
+     <h1> Final Results!!! </h1>
+     <h2> {score} out of {questions.length} correct! </h2>
+     <button onClick = {() => restartGame()}>Practice Again </button>
     </div>
+    
+    :
+
+     /*3. Question*/
+     <div className = 'questions'>
+      <h2>Question {currentQ + 1} of {questions.length} 
+      {" ---- " + questions[currentQ].title}
+      {" ---- " + questions[currentQ].category}</h2>
+      <h3 className = "questionTitle">
+      {questions[currentQ].text}</h3>
+
+      <ul>
+            {questions[currentQ].options.map((option) => {
+              return (
+                <li className = "options"
+                  key={option.id}
+                  onClick={() => optionClicked(option.isCorrect)}
+                  /*Needs to be here to refrain from printing this as text ^ */
+                >
+                  {option.text}
+                </li>
+              );
+            })}
+          </ul>
+     </div>
+    }
+
+  </div>
   );
 }
 
---
+++ src/QuestionList.js
@@ -0,0 +1,222 @@
+/*Questions from quizdb*/
+export const questions = [
+    {
+      category: "Fine Arts",
+      title: "BONUS 1a",
+      text: "Saint James blesses a man on his way to his martyrdom in a work by " +
+      "this artist shown from a worm's eye perspective that was destroyed during World War II. " +
+      "FOR 10 POINTS EACH: Identify this Renaissance artist whose ceiling oculus " + 
+      "in the Palazzo Ducale in Mantua depicts several cherubs sitting on the roof beneath a blue sky.",
+      options: [
+        { id: 0, text: "Mantegna", isCorrect: true },
+        { id: 1, text: "Titian", isCorrect: false },
+        { id: 2, text: "Botticelli", isCorrect: false },
+        { id: 3, text: "Donatello", isCorrect: false },
+      ]
+    },
+    {
+      category: "Fine Arts",
+      title: "BONUS 1b",
+      text: "Saint James blesses a man on his way to his martyrdom in a work by " +
+      "this artist shown from a worm's eye perspective that was destroyed during World War II. " +
+      "FOR 10 POINTS EACH: Andrea Mantegna produced a heavily foreshortened painting of the dead body of " +
+      "this Biblical figure that illustrated his Holy Wounds. He can be seen resting in Mantegna's " +
+      "Madonna with Sleeping Child.",
+      options: [
+        { id: 0, text: "Simon", isCorrect: false },
+        { id: 1, text: "Peter", isCorrect: false },
+        { id: 2, text: "Jesus", isCorrect: true },
+        { id: 3, text: "Judas", isCorrect: false },
+      ]
+    },
+    {
+      category: "Fine Arts",
+      title: "BONUS 1c",
+      text: "Saint James blesses a man on his way to his martyrdom in a work by" +
+      "this artist shown from a worm's eye perspective that was destroyed during World War II." +
+      "FOR 10 POINTS EACH: Mantegna created multiple scenes of this figure, often shown being " +
+      "martyred while tied up near a column. El Greco painted this saint tied up to a tree with " +
+      "an arrow piercing his ribs.",    
+      options: [
+        { id: 0, text: "Michael", isCorrect: false },
+        { id: 1, text: "Joseph", isCorrect: false },
+        { id: 2, text: "Anthony", isCorrect: false },
+        { id: 3, text: "Sebastian", isCorrect: true },
+      ]
+    },
+
+
+    {
+      category: "Science",
+      title: "BONUS 2a",
+      text: "These structures are called “protoplanetary” when they " +
+      "surround T Tauri stars. FOR 10 POINTS EACH: " +
+      "Name these agglomerations of gas, dust, or other matter that orbit a star in the shape of a torus or ring.",
+      options: [
+        { id: 0, text: "Accretion Circles", isCorrect: false },
+        { id: 1, text: "Debris Spheres", isCorrect: false },
+        { id: 2, text: "Debris Circles", isCorrect: false },
+        { id: 3, text: "Circumstellar Disks", isCorrect: true },
+      ]
+    },
+    {
+      category: "Science",
+      title: "BONUS 2b",
+      text: "These structures are called “protoplanetary” when they " +
+      "surround T Tauri stars. FOR 10 POINTS EACH: " +
+      "This circumstellar disk located in our solar system extends outward from the orbit " +
+      "of Neptune and contains asteroids, short-period comets, and dwarf planets such as Pluto.",
+      options: [
+        { id: 0, text: "Kander Belt", isCorrect: false },
+        { id: 1, text: "Kirkwood Belt", isCorrect: false },
+        { id: 2, text: "Kuiper Belt", isCorrect: true },
+        { id: 3, text: "Kisner Belt", isCorrect: false },
+      ]
+    },
+    {
+      category: "Science",
+      title: "BONUS 2c",
+      text: "These structures are called “protoplanetary” when they " +
+      "surround T Tauri stars. FOR 10 POINTS EACH: " +
+      "These objects are considered “classical” Kuiper belt objects and are often contrasted " +
+      "with “resonant” plutinos. These objects are named for the provisional designation of Albion, " +
+      "the first of them to be discovered.",  
+      options: [
+        { id: 0, text: "Cubewanos", isCorrect: true },
+        { id: 1, text: "Centaurs", isCorrect: false },
+        { id: 2, text: "Comets", isCorrect: false },
+        { id: 3, text: "Asteroids", isCorrect: false },
+      ]
+    },
+
+
+    {
+      category: "Fine Arts",
+      title: "BONUS 3a",
+      text: "This painting, now housed in the MoMA, shows a man with a goatee who holds " +
+      "both a top hat and a flower. FOR 10 POINTS EACH: " +
+      "Name this painting, where the title figure stands in front of several fields of color that spiral out from the center.",
+      options: [
+        { id: 0, text: "Portrait of Félix Fénéon", isCorrect: true },
+        { id: 1, text: "Antibes", isCorrect: false },
+        { id: 2, text: "Capo di Noli", isCorrect: false },
+        { id: 3, text: "Breakfast", isCorrect: false },
+      ]
+    },
+    {
+      category: "Fine Arts",
+      title: "BONUS 3b",
+      text: "This painting, now housed in the MoMA, shows a man with a goatee who holds " +
+      "both a top hat and a flower. FOR 10 POINTS EACH: " +
+      "Paul Signac, the painter of Portrait of Félix Fénéon, helped start this art movement, along with Georges Seurat. " +
+      "It uses several small dots of color that together form an image.",
+      options: [
+        { id: 0, text: "Color Theory", isCorrect: false },
+        { id: 1, text: "Pointilism", isCorrect: true },
+        { id: 2, text: "Dotting", isCorrect: false },
+        { id: 3, text: "Pointily", isCorrect: false },
+      ]
+    },
+    {
+      category: "Fine Arts",
+      title: "BONUS 3c",
+      text: "This painting, now housed in the MoMA, shows a man with a goatee who holds " +
+      "both a top hat and a flower. FOR 10 POINTS EACH: " +
+      "This painting by Seurat shows several men reclining along the banks of a river, including " +
+      "one who wears only red trunks and another with a red hat on his head.",
+      options: [
+        { id: 0, text: "Beach at Bas Butin, Honfleur", isCorrect: false },
+        { id: 1, text: "Port-en-Bessin, Sunday", isCorrect: false },
+        { id: 2, text: "Bathers at Asnières", isCorrect: true },
+        { id: 3, text: "Bridge at Courbevoie", isCorrect: false },
+      ]
+    },
+  
+  
+    {
+      category: "History",
+      title: "BONUS 4a",
+      text: "Robert Caro won a Pulitzer Prize for profiling this man in the critical 1974 biography The Power Broker. " +
+      "FOR 10 POINTS EACH: " +
+      "Name this influential New York urban planner of the mid-20th century. He organized and planned projects like the Triborough " +
+      "and Verrazzano–Narrows bridges, Lincoln Center, and Shea Stadium.",
+      options: [
+        { id: 0, text: "Robert J. Gibbs", isCorrect: false },
+        { id: 1, text: "Charles Marohn", isCorrect: false },
+        { id: 2, text: "Robert Moses", isCorrect: true },
+        { id: 3, text: "Donald Shoup", isCorrect: false },
+      ]
+    },
+    {
+      category: "History",
+      title: "BONUS 4b",
+      text: "Robert Caro won a Pulitzer Prize for profiling this man in the critical 1974 biography The Power Broker. " +
+      "FOR 10 POINTS EACH: " +
+      "One of Moses’s biggest achievements was bringing the headquarters of this intergovernmental organization to New York. " +
+      "This organization’s General Assembly and Security Council convene in a complex on the East River.",
+      options: [
+        { id: 0, text: "World Trade Organization", isCorrect: false },
+        { id: 1, text: "United Nations", isCorrect: true },
+        { id: 2, text: "International Labour Organization", isCorrect: false },
+        { id: 3, text: "World Bank", isCorrect: false },
+        ]
+    },
+    {
+      category: "History",
+      title: "BONUS 4c",
+      text: "Robert Caro won a Pulitzer Prize for profiling this man in the critical 1974 biography The Power Broker. " +
+      "FOR 10 POINTS EACH: " +
+      "Moses’s rise to power was aided by this governor of New York who was nicknamed the “Happy Warrior.” " +
+      "Anti-Catholic sentiment contributed to this man’s loss in the 1928 presidential election.",
+      options: [
+        { id: 0, text: "Thomas Davis", isCorrect: false },
+        { id: 1, text: "Nick Johnson", isCorrect: false },
+        { id: 2, text: "Jack Brown", isCorrect: true },
+        { id: 3, text: "Al Smith", isCorrect: true },
+      ]
+    },
+
+
+    {
+      category: "Literature",
+      title: "BONUS 5a",
+      text: "Benjamin Saxton argued that this author broke down the “omnipotent and omnipresent” God of Roland Barthes " +
+      "into one that “leaves space for the creativity and agency of his subjects.” " +
+      "FOR 10 POINTS EACH: Name this author. In his “Gospel According to” this author, Ralph C. Wood argued that figures outside " +
+      "“formal religion” such as Eru Ilúvatar represent a deep Christian allegory in this author’s work.",
+      options: [
+        { id: 0, text: "Terry Pratchett", isCorrect: false },
+        { id: 1, text: "J. R. R. Tolkien", isCorrect: true },
+        { id: 2, text: "N.K. Jemisin", isCorrect: false },
+        { id: 3, text: "Neil Gaiman", isCorrect: false },
+      ]
+    },
+    {
+      category: "Literature",
+      title: "BONUS 5b",
+      text: "Benjamin Saxton argued that this author broke down the “omnipotent and omnipresent” God of Roland Barthes " +
+      "into one that “leaves space for the creativity and agency of his subjects.” " +
+      "FOR 10 POINTS EACH: Wood recognized the symbolism of this figure as a “Jesus Christ” of Middle Earth. This legendary Istari wizard "+
+      "rescues the Hobbits from Mount Doom with giant eagles after leading armies against Sauron at the Black Gate.",
+      options: [
+        { id: 0, text: "Gandalf", isCorrect: false },
+        { id: 1, text: "Radagast", isCorrect: false },
+        { id: 2, text: "Saruman", isCorrect: true },
+        { id: 3, text: "Pallando", isCorrect: false },
+      ]
+    },
+    {
+      category: "Literature",
+      title: "BONUS 5c",
+      text: "Benjamin Saxton argued that this author broke down the “omnipotent and omnipresent” God of Roland Barthes " +
+      "into one that “leaves space for the creativity and agency of his subjects.” " +
+      "FOR 10 POINTS EACH: Tolkien adapted Judeo-Christian myths such as the Great Flood into events like the Fall of Númenor " +
+      "in this seminal collection of Middle Earth mythology.",    
+      options: [
+        { id: 0, text: "Beren and Lúthien", isCorrect: false },
+        { id: 1, text: "The Silmarillion", isCorrect: true },
+        { id: 2, text: "Lord of the Rings", isCorrect: false },
+        { id: 3, text: "The Children of Húrin", isCorrect: false },
+      ]
+    }
+  ];
